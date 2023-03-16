a06a86c109f6fcc47eb577e4b493a2736d14426e
James Anderson <jamesanderson6271@gmail.com>
2023-03-16 12:39:09 -0600

    Oasis Build (2/16)

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# Welcome to Streamlit!
+# Welcome to Oasis!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
+This mockup was creating using Streamlit. This service allows us to create functional webpages without front-end expertise.  
+
+:heart:		File Path -->		:heart:
+
+
 
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,12 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Welcome to Oasis!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+This mockup was creating using Streamlit. This service allows us to create functional webpages without front-end expertise.  
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+:heart:     File Path -->  '/streamlit_app.py'     :heart:
+
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
