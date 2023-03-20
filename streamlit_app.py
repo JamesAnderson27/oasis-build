b2dcb2c610aa5135031819bd864bb821e4b7a35d
James Anderson <jamesanderson6271@gmail.com>
2023-03-20 17:22:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-st.title('      :evergreen_tree:  :green[Welcome to Oasis!] :evergreen_tree:')
+style = "<style>h1 {text-align: center;}</style>"
+st.markdown(style, unsafe_allow_html=True)
+st.columns(3)[1].header(":evergreen_tree:  :green[Welcome to Oasis!] :evergreen_tree:")
 
 st.write('We provide a centralized pool of public information to inform your next trip into the *Great* Outdoors.')
-
-
-style = "<style>h2 {text-align: center;}</style>"
-st.markdown(style, unsafe_allow_html=True)
-st.columns(3)[1].header("hello world")
-
 
 with open('PRODUCT_foliummap.html','r') as f:
     html_read = f.read()
