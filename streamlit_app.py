3102cfbe08fff3a6fe1e2bbe6839f3e859de0251
James Anderson <jamesanderson6271@gmail.com>
2023-03-20 16:31:32 -0600

    Oasis Build

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 # :evergreen_tree: Welcome to Oasis! :evergreen_tree:
 """
 
-st.write('We provide a centralized pool of public information to inform your next trip into the *great* outdoors.')
+st.write('We provide a centralized pool of public information to inform your next trip into the *Great* Outdoors.')
 
 with open('PRODUCT_foliummap.html','r') as f:
     html_read = f.read()
@@ -12,3 +12,9 @@ with open('PRODUCT_foliummap.html','r') as f:
 st.header("Prototype Map")
 st.components.v1.html(html_read,height=400)
 
+st.write("Here's our first attempt at using data to create a table:")
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+st.write(df)
