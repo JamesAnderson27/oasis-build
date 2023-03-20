c7605064bb9f71ee3c73131059aec0b9f1fa19ed
James Anderson <jamesanderson6271@gmail.com>
2023-03-20 17:56:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,14 @@ st.title('      :evergreen_tree:  :green[Welcome to Oasis!] :evergreen_tree:')
 st.write('We provide a centralized pool of public information to inform your next trip into the *Great* Outdoors.')
 
 
-
+# DISPLAY FOLIUM MAP
 with open('PRODUCT_foliummap.html','r') as f:
     html_read = f.read()
 st.header("Prototype Map")
 st.components.v1.html(html_read,height=400)
 
 
-
+# PRACTICE TABLE
 st.write("Here's our first attempt at using data to create a table:")
 df = pd.DataFrame({
     'first column': [1, 2, 3, 4],
@@ -23,15 +23,16 @@ df = pd.DataFrame({
 st.write(df)
 
 
-
-site_code = ' '
+# CAMPSITE SELECT AND DISPLAY
 site_code = st.number_input('Enter a campsite code',
     min_value=int(0),
     max_value=int(10),
     step=1)
-
-st.write('The current campsite is ', site_code)
-
+st.write('The selected campsite is ', site_code)
 message_list = ['You picked campsite #1!','You picked campsite #2!']
-if st.button('Click for details.'):
+if st.button('Click for details'):
     st.write('You selected: ',message_list[int(site_code)])
+
+
+
+# 
