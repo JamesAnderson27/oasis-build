056f7df30634bb23e84e5d5bea1de59f783a0fdd
James Anderson <jamesanderson6271@gmail.com>
2023-03-20 16:19:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,7 @@ import streamlit as st
 # :evergreen_tree: Welcome to Oasis! :evergreen_tree:
 """
 
-st.write(We provide a centralized pool of public information to 
-    inform your next trip into the *great* outdoors.)
+st.write('We provide a centralized pool of public information to inform your next trip into the *great* outdoors.')
 
 with open('PRODUCT_foliummap.html','r') as f:
     html_read = f.read()
