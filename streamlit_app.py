616fd768e03e135b2b786ca14d6875c950faa835
James Anderson <jamesanderson6271@gmail.com>
2023-03-20 16:12:27 -0600

    Oasis Build

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 """
 # :evergreen_tree: Welcome to Oasis! :evergreen_tree:
 
-We provide a centralized pool of public information to inform your next trip into the *great* outdoors.
+###We provide a centralized pool of public information to inform your next trip into the *great* outdoors.
 """
 
 with open('PRODUCT_foliummap.html','r') as f:
