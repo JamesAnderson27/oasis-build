23795498658582294974f6cb2a323f466ea71013
James Anderson <jamesanderson6271@gmail.com>
2023-03-20 18:09:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-
+# HEADER
 st.title('      :evergreen_tree:  :green[Welcome to Oasis!] :evergreen_tree:')
 st.write('We provide a centralized pool of public information to inform your next trip into the *Great* Outdoors.')
 
@@ -12,15 +12,6 @@ with open('PRODUCT_foliummap.html','r') as f:
     html_read = f.read()
 st.header("Prototype Map")
 st.components.v1.html(html_read,height=400)
-
-
-# PRACTICE TABLE
-st.write("Here's our first attempt at using data to create a table:")
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-})
-st.write(df)
 
 
 # CAMPSITE SELECT AND DISPLAY
@@ -33,6 +24,16 @@ message_list = ['You picked campsite #1!','You picked campsite #2!']
 if st.button('Click for details'):
     st.write('You selected: ',message_list[int(site_code)])
 
+    # PLACE HOLDER FOR DB READING
+    site_name = 'Cimarron Campground'
+    site_alerts = ['Black Bears in the area.']
+    site_weather = {'prec_type':':sun:'}
+    site_coord = (23.3253,75.3452)
+
+    st.subheader(site_name)
+    st.code(site_coord)
+    st.write('Daily Forecast: ', site_weather['prec_type'])
+    st.write('Daily Alerts: ', site_alerts)
 
 
-# 
+
