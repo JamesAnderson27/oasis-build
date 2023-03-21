866288a2912b68c891dda8c89fe5a2409b4cd234
James Anderson <jamesanderson6271@gmail.com>
2023-03-20 21:22:54 -0600

    Oasis Build

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,14 +27,15 @@ st.components.v1.html(html_read,height=400)
 
 
 # CAMPSITE SELECT AND DISPLAY
-site_code = st.number_input('Enter a campsite code',
-    min_value=int(0),
-    max_value=int(10),
+site_code = st.number_input('Enter campsite number',
+    min_value=int(10),
+    max_value=int(39),
     step=1)
-st.write('The selected campsite is ', site_code)
-message_list = ['You picked campsite #1!','You picked campsite #2!']
+st.write('Selected campsite:', site_code)
+
+st.table(site_df)
+
 if st.button('Click for details'):
-    st.write('You selected: ',message_list[int(site_code)])
 
     # PLACE HOLDER FOR DB READING
     site_name = 'Cimarron Campground'
