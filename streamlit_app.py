9d97cd8c50ed11f1120875433e38245ff4c5eff0
James Anderson <jamesanderson6271@gmail.com>
2023-04-17 16:14:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,103 +9,7 @@ import json
 
 ## LIVE SCRAPING - NPS
 
-# Site Scrape
-api_key='&api_key=Vsv3OfLMOGtoptQS7V5zvYIKawJZ29UbTErkVLgl'
-url = 'https://developer.nps.gov/api/v1/campgrounds?stateCode=co'
-limits='&start=0&limit=400'
-r = requests.get(url+limits+api_key)
-content = r.content
-data = json.loads(content.decode('utf-8'))
-
-name=[]
-lat=[]
-long=[]
-p_code=[]
-rez_type=[]
-rez_link=[]
-road = []
-fee = []
-trash = []
-toilets = []
-water = []
-wood = []
-
-for park in data["data"]:
-    name.append(park["name"])
-    lat.append(float(park["latitude"]))
-    long.append(float(park["longitude"]))
-    p_code.append(str(park["parkCode"]))
-    road.append(park['accessibility']['accessRoads'])
-    trash.append(park['amenities']['trashRecyclingCollection'])
-    toilets.append(park['amenities']['toilets'][0])
-    water.append(park['amenities']['potableWater'][0])
-    wood.append(park['amenities']['firewoodForSale'])
-    
-
-    if len(park['fees'])>=1:
-        fee.append(float(park['fees'][0]['cost']))
-    else:
-        fee.append('No Fee Recorded')
-    
-    
-    n_res = int(park['numberOfSitesReservable'])
-    n_fc = int(park['numberOfSitesFirstComeFirstServe'])
-    
-    if n_fc>0:
-        rez_type.append('First-come-first-serve')
-        rez_link.append(park['reservationUrl'])
-    
-        
-    else:
-        rez_type.append('Reservations Available')
-        rez_link.append(park['reservationUrl'])
-
-n = len(data['data'])
-ids = np.arange(10,10+n)
-
-site_dict={
-    "id":ids,
-    "park_name":name,
-    "latitude":lat,
-    "longitude":long,
-    "park_code":p_code,
-    "reserve":rez_type,
-    "reserve_link":rez_link,
-    "fee":fee,
-    "road_conditions":road,
-    "firewood":wood,
-    "water":water,
-    "trash":trash,
-    "toilets":toilets
-}
-
-site_df = pd.DataFrame(site_dict)
-
-# Alert Scrape
-api_key='&api_key=Vsv3OfLMOGtoptQS7V5zvYIKawJZ29UbTErkVLgl'
-url = 'https://developer.nps.gov/api/v1/alerts?stateCode=co'
-limits='&start=0&limit=50'
-r = requests.get(url+limits+api_key)
-content = r.content
-data = json.loads(content.decode('utf-8'))
-
-title=[]
-park_code=[]
-message=[]
-
-for i,item in enumerate(data['data']):
-    title.append(item['title'])
-    park_code.append(item['parkCode'])
-    message.append(item['description'])
-    
-assert len(park_code)==len(message)==len(title)
-
-alert_dict = {
-    'title':title,
-    'park_code':park_code,
-    'message':message
-}
-alert_df = pd.DataFrame(alert_dict)
+## PLACEHOLDER
 
 # HEADER
 st.title('      :evergreen_tree:  :green[Welcome to Oasis!] :evergreen_tree:')
@@ -113,8 +17,8 @@ st.write('We provide a centralized pool of public information to inform your nex
 st.write('**Directions: Select a campsite from the map and type the site ID into the box below.**\n')
 
 # READ IN DB
-#alert_df = pd.read_csv('DB_alerts.csv')
-#site_df = pd.read_csv('DB_campsites.csv')
+alert_df = pd.read_csv('DB_alerts.csv')
+site_df = pd.read_csv('DB_campsites.csv')
 
 
 # DISPLAY FOLIUM MAP
@@ -135,20 +39,20 @@ st.write('Selected campsite:', site_code)
 if st.button('Click for details'):
 
     site = site_df[site_df['id']==site_code].values[0]
-    site_name = site[1]
+    site_name = site[2]
 
-    site_coord = (site[2],site[3])
-    p_code=site[4]
-    rez_type=site[5]
-    rez_link=site[6]
-    fee=site[7]
-    road_c = site[8].strip("'[]")
+    site_coord = (site[3],site[4])
+    p_code=site[5]
+    rez_type=site[6]
+    rez_link=site[7]
+    fee=site[8]
+    road_c = site[9].strip("'[]")
 
     am_dict = {}
-    firewood = site[9]
-    water = site[10]
-    trash = site[11]
-    toilets = site[12]
+    firewood = site[10]
+    water = site[11]
+    trash = site[12]
+    toilets = site[13]
 
     st.subheader('\n')
     st.header(str(site_name))
